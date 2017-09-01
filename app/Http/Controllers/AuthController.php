3dd49bb1465b69bbbc55408c55740f6e72362a5c
myName005 <eer37779@gmail.com>
2017-09-01 19:50:44 +0100

    add login controllers and users table seeder

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Auth;
+class AuthController extends Controller
+{
+	function __construct()
+	{
+		$this->middleware('guest');
+	}
+	public function loginPage()
+	{
+		return view('auth.login');
+	}
+
+    public function login(Request $request)
+    {
+    	$this->validate($request, [
+	        'email' => 'required',
+	        'password' => 'required|min:6',
+	    ]);
+
+	    $credentials = [
+	    	'email' => $request->input('email'),
+	    	'password' => $request->input('password'),
+	    ];
+
+    	if(Auth::attempt($credentials))
+    		return redirect('/');
+
+    	return redirect()->back()
+    	->withErrors('wrong password/email')->withInput();
+    }
+}
