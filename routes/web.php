c1ff4347e9bc9fffbf2b64d68f1b17b9b8ae173c
myName005 <eer37779@gmail.com>
2017-09-03 07:44:24 +0100

    add names to each route

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,30 @@
 */
 Route::middleware('guest')->group(function ()
 {
-	Route::view('/login','auth.login');
-	Route::post('/login','AuthController@login');
+	Route::view('/login','auth.login')
+		->name('login_page');
+	Route::post('/login','AuthController@login')
+		->name('login');
 
-	Route::view('/signin','auth.signin');
-	Route::post('/signin','AuthController@signin');
+	Route::view('/signin','auth.signin')
+		->name('signin_page');
+	Route::post('/signin','AuthController@signin')
+		->name('signin_page');
 });
 
 
-Route::view('/make_article','article.make');
-Route::post('/make_article','ArticleController@make');
+Route::view('/make_article','article.make')
+	->name('make_article_page');
+Route::post('/make_article','ArticleController@make')
+	->name('make_article');
 
-Route::get('/show_article/{id}','ArticleController@show')->name('show_article');
+Route::get('/show_article/{id}','ArticleController@show')
+	->name('show_article');
 
-Route::get('/', 'HomePageController@index');
+Route::get('/edit_article/{id}','ArticleController@editPage')
+	->name('edit_article_page');
+Route::post('/edit_article/{id}','ArticleController@edit')
+	->name('edit_article');
+
+Route::get('/', 'HomePageController@index')
+	->name('home_page');
