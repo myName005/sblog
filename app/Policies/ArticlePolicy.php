da0bf7bbfd28ccd34fb3be99c27fe85321498018
myName005 <eer37779@gmail.com>
2017-09-03 11:07:45 +0100

    add article authorization policies

---
+++ app/Policies/ArticlePolicy.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Article;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ArticlePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the article.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+    public function view(User $user, Article $article)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can make articles.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function make(User $user)
+    {
+        return  $user->role=='admin'    || 
+                $user->role=='editor'   ||
+                $user->role=='author';
+    }
+
+    /**
+     * Determine whether the user can edit the article.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+    public function edit(User $user, Article $article)
+    {
+            
+        if($user->role=='admin'|| $user->role=='editor')
+            return true;
+
+        if( $user->role == 'author' &&
+            $user->id == $post->author_id)
+            return true;
+
+        return false;
+
+    }
+
+    /**
+     * Determine whether the user can delete the article.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+    public function delete(User $user, Article $article)
+    {
+        if($user->role=='admin'|| $user->role=='editor']))
+            return true;
+        if($user->role == 'author' && $user->id == $post->author_id)
+            return true;
+
+        return false;
+    }
+}
