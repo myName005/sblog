b434552d5dfcbc32420e0bc417e0ea8bb1e18d3c
myName005 <eer37779@gmail.com>
2017-09-03 08:00:51 +0100

    add edit article feature

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,18 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use \Auth;
 
 use App\Article;
-use \Auth;
 
 class ArticleController extends Controller
 {
+    /*
+    | make article 
+    |
+    */ 
     public function make(Request $request)
     {
     	$this->validate($request,[
     		'title'=> 'required|max:255',
     		'content'=>'required'
     	]);
+
     	$article = new Article();
     	$article->title = $request->input('title');
     	$article->content = $request->input('content');
@@ -26,6 +32,10 @@ class ArticleController extends Controller
     	return redirect()->route('show_article',['id'=>$article->id]);
     }
 
+    /*
+    | show article 
+    |
+    */ 
     public function show($id)
     {
     	$article = Article::find($id);
@@ -34,4 +44,48 @@ class ArticleController extends Controller
 
     	return view('article.show', ['article'=>$article]);
     }
+
+
+    /*
+    | edit article 
+    |
+    */ 
+    public function editPage($id)
+    {
+        $article = Article::find($id);
+        if($article == null)
+            abort(404);
+
+        $input = ['article'=>$article];
+        return view('article.edit',$input); 
+    }
+
+    public function edit($id,Request $request)
+    {
+        $article = Article::find($id);
+        
+        if($article == null)
+            abort(404);
+        
+        $validator = Validator::make($request->all(),[
+            'title'=> 'required|max:255',
+            'content'=>'required'
+        ]);
+        
+        $article->title = $request->input('title');
+        $article->content = $request->input('content');
+        
+        if($validator->fails())
+        {
+            $input = ['article'=>$article];
+            return view('article.edit',$input)
+                ->withErrors($validator);
+        }
+
+        if(!$article->save())
+            abort(500);
+        
+        return redirect()->route('show_article',['id'=>$id]);
+    }
+
 }
---
+++ resources/views/article/edit.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit</title>
+</head>
+<body>
+<form action="{{route('edit_article', ['id'=>$article->id])}}" method="POST">
+	{{ csrf_field() }}
+
+	
+	<input type="text" name="title"
+	value="{{ $article->title }}">
+
+	<textarea 
+	name="content">{{ $article->content }}</textarea>
+	<button type="submit">Submit</button>
+	@if ($errors->any())
+		<div style="color:#f66" class="alert alert-danger">
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif
+</form>
+</body>
+</html>
