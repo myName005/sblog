84c6bcaa530f9e9ae52cb6333c4e9e5498566a91
myName005 <eer37779@gmail.com>
2017-09-04 18:36:32 +0100

    revert tag commits

--- app/Article.php
+++ app/Article.php
@@ -6,13 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-	public function author()
-	{
-		return $this->belongsTo('App\User','author_id');
-	}
-
-	public function tags()
-	{
-		return $this->belongsToMany('App\Tag');
-	}
+	
 }
