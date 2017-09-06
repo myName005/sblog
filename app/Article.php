0b25ef7bcb9244afe6a37050c85fd9319978544f
myName005 <eer37779@gmail.com>
2017-09-06 12:46:50 +0100

    set relations between Article and Image

--- app/Article.php
+++ app/Article.php
@@ -15,4 +15,9 @@ class Article extends Model
 	{
 		return $this->belongsTo('App\Catigory','catigory_id');
 	}
+
+	public function images()
+	{
+		return $this->morphMany('App\Image','imageable');
+	}
 }
