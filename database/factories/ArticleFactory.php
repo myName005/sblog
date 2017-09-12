f06a40140f0073e1937ea21a2f6ef05bc470af89
myName005 <eer37779@gmail.com>
2017-09-12 11:16:14 +0100

    improve seeding

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -6,7 +6,9 @@ $factory->define(App\Article::class, function (Faker $faker) {
     return [
         'title' => $faker->text(50),
         'content' => $faker->text(1000),
-        'author_id' => rand()%50+1,
-        'catigory_id'=> rand()%10+1
+        'author_id' => App\User::whereIn('role',
+        	['author','admin','editor'])->get()->random()->id,
+        'catigory_id'=> App\Catigory::all()->random()->id
+
     ];
 });
