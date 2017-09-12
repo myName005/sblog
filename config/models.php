8207399df97d076ac656c6694fad2dfaecc717c0
myName005 <eer37779@gmail.com>
2017-09-12 17:59:06 +0100

    add conifig file for models

---
+++ config/models.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+
+    'user' => [
+    	'seeding'=>[
+    		'count'=>50
+    	],
+    ],
+    'article' => [
+    	'seeding'=>[
+    		'count'=>100
+    	],
+    ],
+    'catigory' => [
+    	'seeding'=>[
+    		'count'=>10
+    	],
+    ],
+    'images' => [
+    	'directory'=>'public/images/'
+    	'seeding'=>[
+    		'count'=>500,
+    		'files_count'=>100
+    	],
+    ],
+];
