f7992dd596f2cd85c37bba3385fb236d11241e0e
myName005 <eer37779@gmail.com>
2017-09-15 10:49:26 +0100

    add "number of artile per page" to models config file

--- config/models.php
+++ config/models.php
@@ -7,16 +7,25 @@ return [
     	'seeding'=>[
     		'count'=>50
     	],
+        'pagination'=>[
+            'count'=>20
+        ]
     ],
     'article' => [
     	'seeding'=>[
     		'count'=>100
     	],
+        'pagination'=>[
+            'count'=>12
+        ]
     ],
     'catigory' => [
     	'seeding'=>[
     		'count'=>10
     	],
+        'pagination'=>[
+            'count'=>12
+        ]
     ],
     'image' => [
     	'directory'=>'public/images/',
