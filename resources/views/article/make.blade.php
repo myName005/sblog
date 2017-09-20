f1c38b6e7d7705d7f9b1bace2f376b0fdab83920
myName005 <eer37779@gmail.com>
2017-09-20 19:54:57 +0100

    style make_article view

--- resources/views/article/make.blade.php
+++ resources/views/article/make.blade.php
@@ -1,25 +1,82 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Write Article</title>
-</head>
-<body>
-	<form action="/make_article" method="post" enctype="multipart/form-data">
+
+@extends('layouts.app')
+
+@section('scripts')
+<script src="{{ asset('js/article.js') }}"></script>
+@endsection
+
+@section('content')
+<div class="columns">
+<column class="column is-8 is-offset-2">
+<div class="box">
+	<form action="/make_article" method="post" 
+	 	class="form" enctype="multipart/form-data">
 		{{ csrf_field() }}
 		
-		<input type="text" name="title" ><br>
+		<div class="field">
+			<div class="lable">Title</div>
+			<div class="control">
+				<input 
+					class="input" 
+					type="text" 
+					name="title" 
+					placeholder="Title">
+			</div>
+		</div>
 		
-		<textarea name="content" rows="7" width="500"></textarea><br>
+		<div class="field">
+			<div class="lable">Content</div>
+			<div class="control">
+				<textarea 
+					class="textarea" 
+					name="content" 
+					rows="7"
+					placeholder="Article content , you can use markdown"
+					></textarea>
+			</div>
+		</div>
 		
-		<select name="catigory">
-			@foreach($catigories as $catigory)
-				<option value="{{$catigory->id}}">{{$catigory->name}}</option>
-			@endforeach
-		</select><br>
 		
-		<input type="file" name="image" accept="image/*"><br>
+		<div class="field">
+			<div class="lable">Catigory</div>
+			<div class="control">
+				<div class="select">
+				<select name="catigory">
+					@foreach($catigories as $catigory)
+						<option value="{{$catigory->id}}">{{$catigory->name}}</option>
+					@endforeach
+				</select>
+				</div>
+			</div>
+		</div>
 
-		<button type="submit">Submit</button>
+		<div class="field">
+			<div class="lable">Image</div>
+			<div class="control">
+<div class="file">
+	<label class="file-label">
+		<input class="file-input" type="file" name="image" accept="image/*">
+		<span class="file-cta">
+			<span class="file-icon">
+				<i class="fa fa-upload"></i>
+			</span>
+			<span class="file-label">
+				Choose an image file…
+			</span>
+		</span>
+	</label>
+</div>
+			</div>
+		</div>
+		
+		
+		
+		<div class="field">
+			<div class="control">
+				<button class="button is-primary" type="submit">Submit</button>
+			</div>
+		</div>
+		
 
 
 
@@ -33,5 +90,8 @@
 			</div>
 		@endif
 	</form>
-</body>
-</html>
+</div>
+</column>
+</div>
+
+@endsection
