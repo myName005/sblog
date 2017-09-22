06e88e9afa3183f3c1e1e2e9a6328b11d5e5c034
myName005 <eer37779@gmail.com>
2017-09-22 12:51:58 +0100

    add style to edit page view

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -1,38 +1,123 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Edit</title>
-</head>
-<body>
-<form action="{{route('edit_article', ['id'=>$article->id])}}" method="POST" enctype="multipart/form-data">
-	{{ csrf_field() }}
 
-	<input type="text" name="title"
-	value="{{ $article->title }}"><br>
+@extends('layouts.app')
 
-	<textarea 
-	name="content">{{ $article->content }}</textarea><br>
 
-	<select name="catigory">
-		@foreach($catigories as $catigory)
-			<option value="{{$catigory->id}}">{{$catigory->name}}</option>
-		@endforeach
-	</select><br>
-	<input type="radio" name="edit_image" value="overwrite">Overwrite previous image
-	<input type="radio" name="edit_image" value="keep" checked>keep previous image <br>
-	<input type="file" name="image" accept="image/*" ><br>
 
-	<button type="submit">Submit</button><br>
-	
-	@if ($errors->any())
-		<div style="color:#f66" class="alert alert-danger">
-			<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
+@section('content')
+<div class="columns">
+<column class="column is-8 is-offset-2">
+<div class="box">
+	<form 
+		action="{{route('edit_article', ['id'=>$article->id])}}"
+		method="post" 
+	 	class="form" enctype="multipart/form-data">
+
+		{{ csrf_field() }}
+		
+		<div class="field">
+			<p class="label">Title</p>
+			<div class="control">
+				<input 
+					class="input" 
+					type="text" 
+					name="title" 
+					placeholder="Title"
+					value="{{ $article->title }}">
+			</div>
 		</div>
-	@endif
-</form>
-</body>
-</html>
+		
+		<div class="field">
+			<div class="label">Content</div>
+			<div class="control">
+				<textarea 
+					class="textarea" 
+					name="content" 
+					rows="7"
+					placeholder="Article content , you can use markdown"
+					>{{ $article->content }}</textarea>
+			</div>
+		</div>
+		
+		
+		<div class="field">
+			<div class="label">Catigory</div>
+			<div class="control">
+				<div class="select">
+				<select name="catigory">
+
+					@foreach($catigories as $catigory)
+						<option 
+							value="{{$catigory->id}}"
+							@if($catigory->id == $article->catigory_id)
+								selected="selected" 
+							@endif
+							>{{$catigory->name}}</option>
+					@endforeach
+
+				</select>
+				</div>
+			</div>
+		</div>
+
+		<div class="field">
+			<div class="label">Image</div>
+
+<div class="control">
+	<lable class="radio">
+		<input 
+			type="radio" 
+			name="edit_image" 
+			value="overwrite">
+		Overwrite previous image
+	</lable>
+	<lable class="radio">
+		<input 
+			type="radio" 
+			name="edit_image" 
+			value="keep" 
+			checked>
+		keep previous image	
+	</lable>
+</div>
+
+<div class="control">
+<div class="file">
+	<label class="file-label">
+		<input class="file-input" type="file" name="image" accept="image/*">
+		<span class="file-cta">
+			<span class="file-icon">
+				<i class="fa fa-upload"></i>
+			</span>
+			<span class="file-label">
+				Choose an image file…
+			</span>
+		</span>
+	</label>
+</div>
+</div>
+		</div>
+		
+		
+		
+		<div class="field is-grouped is-grouped-right">
+			<div class="control">
+				<button class="button is-primary" type="submit">Submit</button>
+			</div>
+		</div>
+		
+
+
+
+		@if ($errors->any())
+            <ul class=" notification is-danger">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul> 
+        @endif
+	</form>
+</div>
+</column>
+</div>
+
+@endsection
