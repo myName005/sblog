231118b372370004a55e8304a03602992fce6c6a
myName005 <eer37779@gmail.com>
2017-09-23 14:35:35 +0100

    add style to delete_catigory view

--- resources/views/catigory/delete.blade.php
+++ resources/views/catigory/delete.blade.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Delete catigory</title>
-</head>
-<body>
-<form action="" method="POST">
-	{{ csrf_field() }}
-	do you want to delete this catigory : {{$catigory->name}}
-	<a href="{{ url()->previous()}}">No</a>
-	<button type="submit">Yes</button>
-</form>
-</body>
-</html>
+
+@extends('layouts.app')
+
+
+
+@section('content')
+<div class="columns">
+	<column class="column is-6 is-offset-3">
+		<div class="box  has-text-centered">
+			<form action="" class="form" method="post">
+				{{ csrf_field() }}
+				<h3 class="subtitle">
+					do you want to delete this catigory : {{$catigory->name}}
+				</h3>
+				
+				<div class="controle">
+					<a class="button is-primary " href="{{ url()->previous()}}">No</a>
+					<button class="button is-default" type="submit">Yes</button>
+				</div>
+				
+
+			</form>
+		</div>
+	</column>
+</div>
+@endsection
