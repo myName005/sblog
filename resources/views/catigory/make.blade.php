9794d610eb397a0e51e2b7dee29d8b75646aa961
myName005 <eer37779@gmail.com>
2017-09-23 13:44:54 +0100

    add style to make_catigory view

--- resources/views/catigory/make.blade.php
+++ resources/views/catigory/make.blade.php
@@ -1,22 +1,49 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Make Catigory</title>
-</head>
-<body>
-	<form action="/make_catigory" method="POST">
-		{{ csrf_field() }}
-		<input type="text" name="name">
-		<button type="submit">Submit</button>
-		@if ($errors->any())
-			<div style="color:#f66" class="alert alert-danger">
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-		@endif
-	</form>
-</body>
-</html>
+@extends('layouts.app')
+
+
+
+@section('content')
+<div class="columns">
+	<column class="column is-6 is-offset-3">
+		<div class="box">
+			<form action="/make_catigory" class="form" method="POST">
+				{{ csrf_field() }}
+				<p class="subtitle">
+					Make new Catigory
+				</p>
+				<div class="field">
+					<label class="label">
+						Name
+					</label>
+					<div class="control">
+						<input class="input" type="text" name="name">
+					</div>
+				</div>
+
+				<div class="field">
+					<label class="label">
+						Color
+					</label>
+					<div class="control">
+						<input class="button is-default" type="color" name="color">
+					</div>
+				</div>
+
+				
+				
+				<div class="controle">
+					<button class="button is-primary" type="submit">Submit</button>
+				</div>
+				@if ($errors->any())
+                    <ul class=" notification is-danger">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul> 
+                @endif
+
+			</form>
+		</div>
+	</column>
+</div>
+@endsection
