a75fb370e3ec60070bcb9e3ec655292b7279d3c7
myName005 <eer37779@gmail.com>
2017-09-23 14:27:02 +0100

    add style to catigory_edit view

--- resources/views/catigory/edit.blade.php
+++ resources/views/catigory/edit.blade.php
@@ -1,22 +1,58 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Edit Catigory</title>
-</head>
-<body>
-	<form action="" method="POST">
-		{{ csrf_field() }}
-		<input type="text" name="name" value="{{$catigory->name}}">
-		<button type="submit">Submit</button>
-		@if ($errors->any())
-			<div style="color:#f66" class="alert alert-danger">
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-		@endif
-	</form>
-</body>
-</html>
+@extends('layouts.app')
+
+
+
+@section('content')
+<div class="columns">
+	<column class="column is-6 is-offset-3">
+		<div class="box">
+			<form action="" class="form" method="POST">
+				{{ csrf_field() }}
+				<p class="subtitle">
+					Edit Catigory
+				</p>
+				
+					
+				<div class="field">
+					<label class="label">
+						Name
+					</label>
+					<div class="control">
+						<input class="input" type="text" name="name">
+					</div>
+				</div>
+
+				<div class="field">
+					<label class="label">
+						Color
+					</label>
+					<div class="control">
+						<input class="button is-default" type="color" name="color" value="{{$catigory->color}}">
+					</div>
+				</div>
+
+				
+				<div class="field">
+					<div class="controle">
+						<button class="button is-primary" type="submit">Submit</button>
+					</div>
+				</div>
+				
+
+				@if ($errors->any())
+                    <ul class=" notification is-danger">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @else
+                <div  class="notification is-info">
+					If you <strong>don't</strong> want to change the name leave it <strong>blank</strong> 
+				</div>
+                @endif
+
+			</form>
+		</div>
+	</column>
+</div>
+@endsection
