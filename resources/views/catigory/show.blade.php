297b293d42a4aba75bd4779154aea490b2ee77b0
myName005 <eer37779@gmail.com>
2017-09-23 14:03:20 +0100

    add edit and delete links to catigory_show view

--- resources/views/catigory/show.blade.php
+++ resources/views/catigory/show.blade.php
@@ -3,12 +3,26 @@
 
 
 @section('base_content')
-<div class="hero is-medium is-dark"
+<section class="hero  is-dark"
 	style="border-bottom: 4px solid {{$catigory->color}}">
-	<section class="section">
-		<h1 class="title">{{$catigory->name}}</h1>
-	</section>
-</div>
+		<div class="hero-body">
+			<div class="container">
+				<h1 class="title">{{$catigory->name}}</h1>
+			</div>
+		</div>
+		@can('edit',$catigory)
+			<div class="hero-foot">
+				<nav class="tabs">
+				<div class="container">
+				<ul>
+					<li><a href="{{ route('edit_catigory',['catigory'=>$catigory->id]) }}">Edit</a></li>
+					<li><a href="{{ route('delete_catigory',['catigory'=>$catigory->id]) }}">Delete</a></li>
+				</ul>
+				</div>
+				</nav>
+			</div>
+		@endcan
+</section>
 <section class="section">
 	<div class="container">
 		<div class="columns is-multiline">
